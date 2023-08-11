490a103283dd92b302864be2483c5ff38b67738b
jeff-seyong <sedragon.kim@gmail.com>
2023-08-11 20:25:23 +0900

    #3 - Querydsl 지원을 이용해서 api 검색옵션 추가 `QuerydslPredicateExecutor`, `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- board-service/src/main/java/me/gnoyes/boardservice/repository/ArticleCommentRepository.java
+++ board-service/src/main/java/me/gnoyes/boardservice/repository/ArticleCommentRepository.java
@@ -1,9 +1,30 @@
 package me.gnoyes.boardservice.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import me.gnoyes.boardservice.domain.ArticleComment;
+import me.gnoyes.boardservice.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root) {
+        // ArticleComment의 모든 필드에 대해서 검색 기능이 열려있어서, 모두 닫아준다.
+        bindings.excludeUnlistedProperties(true);
+        // 필요한 검색 기능만 열어준다.
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
