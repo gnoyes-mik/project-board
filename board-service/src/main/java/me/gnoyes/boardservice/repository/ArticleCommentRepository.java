e75723084f90aba360343d4606101628f89c2fa3
jeff-seyong <sedragon.kim@gmail.com>
2023-08-11 20:03:11 +0900

    #3 - data rest 관련 설정

--- board-service/src/main/java/me/gnoyes/boardservice/repository/ArticleCommentRepository.java
+++ board-service/src/main/java/me/gnoyes/boardservice/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package me.gnoyes.boardservice.repository;
 
 import me.gnoyes.boardservice.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
